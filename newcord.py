66addf8181835aca029e69fa6700be7ed8b80487
hosam37r <hosam.amleh@gmail.com>
2021-01-08 10:27:13 -0500

    Update newcord.py

--- newcord.py
+++ newcord.py
@@ -1,5 +1,20 @@
-from geopy.distance import distance
+import math
+from geographiclib.geodesic import Geodesic
+
 def newcord(x,y,xb,yb):
- X = cos(b.lat)* sin(dL)
- Y = cos(a.lat)*sin(b.lat) - sin(a.lat)*cos(b.lat)* cos(dL)
- 
+R = 6378.1 #Radius of the Earth
+brng = Geodesic.WGS84.Inverse(x, y, xb, yb)['azi1']  #bearing
+lat1 = math.radians(x) #Current lat point converted to radians
+lon1 = math.radians(y) #Current long point converted to radians
+d = .005 #Distance in km>> this is assuming the dorne travel 5 m/s
+
+lat2 = math.asin( math.sin(lat1)*math.cos(d/R) +
+     math.cos(lat1)*math.sin(d/R)*math.cos(brng))
+
+lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),
+             math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
+
+lat2 = math.degrees(lat2)
+lon2 = math.degrees(lon2)
+
+return lon2, lat2
