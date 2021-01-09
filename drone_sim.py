3b60bbd3074a7bfe194f2b72bfde0ce548fe1281
hosam37r <hosam.amleh@gmail.com>
2021-01-09 15:33:20 -0500

    Update and rename newcord.py to drone_sim.py

--- drone_sim.py
+++ drone_sim.py
@@ -2,7 +2,12 @@
 import math
 from geographiclib.geodesic import Geodesic # Make sure you install geographiclib  library before running this code.
 
-def newcord(lon,lat,lonb,latb): # Lon, Lat>>current. lonb,latb>> Where the subject is heading ( in our case these are the broadcasted coordniates) 
+def drone_sim(lon,lat): # Lon, Lat>>braodcasted. lonb,latb>> Where the subject is heading ( in our case these are the broadcasted coordniates) 
+     
+     #Launch location of drone. 
+     ## It is hardcoded here. The puprose of this code is to simulate 
+     lon2= -77.873886
+     lat2=34.241753
      
      R = 6378.1 #Radius of the Earth
      brng = Geodesic.WGS84.Inverse(lon, lat, lonb, latb)['azi1']  # Calculate the bearing of the travel
@@ -18,11 +23,11 @@ def newcord(lon,lat,lonb,latb): # Lon, Lat>>current. lonb,latb>> Where the subje
      lat2 = math.degrees(lat2)
      lon2 = math.degrees(lon2)
      
-     return lon2, lat2
+     return brng
 
 
 
 
-newlon,newlat= newcord( -77.920830,34.175038, -77.941166,34.184132) ### You can call the function as you can be seen here. 
+newlon,newlat= drone_sim( -77.920830,34.175038) ### You can call the function as you can be seen here. 
 
 
