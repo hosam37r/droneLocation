1418c8a808d8c37c10761db8145b772efccebc78
hosam37r <hosam.amleh@gmail.com>
2021-01-10 17:52:01 -0500

    Update bisect.py

--- bisect.py
+++ bisect.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python
 from geographiclib.geodesic import Geodesic # Make sure you install geographiclib  library before running this code.
 import math
-
-  ###### below this point is functions
 
 def guessrange(lon,lat): # Lon, Lat>>current.
      R = 6378.1 #Radius of the Earth
@@ -27,80 +24,73 @@ def guessrange(lon,lat): # Lon, Lat>>current.
      minlon = math.degrees(minlon)
 
      return maxlon, maxlat,minlon, minlat
-  ###function 1 bi-section loop to find the lon
-def findx( guess,a,b,f,k):
-  x_speed=[k]
+
+
+def findx( guess,a,b,f):
+  x_speed=[0]
   i=1
   if f==1:
     lower =a
     upper=guess
   else:
     lower=guess
-    print(lower)
     upper=a
-    print(upper)
-
   guess=lower+(upper-lower)/2
-  print(guess)
+
   while True:
     br=drone_sim(guess,b)
-    x_speed.append(5 * math.cos(br))
+    x_speed.append(5 * math.cos(br * math.pi / 180))
     if abs(x_speed[i])<.01:
-      return x_speed,guess
-    if x_speed[i]> x_speed[i-1]:
-      upper=guess
+        return x_speed,guess
+    if x_speed[i]< 0:
+      upper=guess 
       guess=lower+(upper-lower)/2
+
     else:
       lower=guess
       guess=lower+(upper-lower)/2
+
+
     i=i+1
-
- 
-###function 1 bi-section loop to find the lat.
-
+  
 def findy( guess,a,b,f):
   y_speed=[0]
   i=1
   if f==1:
     lower =b
     upper=guess
+
   else:
     lower=guess
     upper=b
+
   guess=lower+(upper-lower)/2
+
   while True:
     br=drone_sim(a,guess)
-    yspeed.append(5 * math.cos(br)) 
+    y_speed.append(5 * math.sin(br * math.pi / 180))
     if abs(y_speed[i])<.01:
-      return y_speed,guess
-    if y_speed[i]> y_speed[i-1]:
-      upper=guess
+        return y_speed,guess
+    if y_speed[i]< 0:
+      upper=guess 
       guess=lower+(upper-lower)/2
+
     else:
       lower=guess
       guess=lower+(upper-lower)/2
+
+
     i=i+1
-      
-## function 3. it finds the maximum range of guesses      
+  
+  
 
-
-      
-    
-    
-    
-    
-## Function 4 to simulate drone movement 
-
-##The prupose of this function is to simulate drone movement. To find which bearing it is going to head to based on the broadcasted Coordinates 
-import math
-from geographiclib.geodesic import Geodesic # Make sure you install geographiclib  library before running this code.
 
 def drone_sim(lon,lat): # Lon, Lat>>braodcasted. lonb,latb>> Where the subject is heading ( in our case these are the broadcasted coordniates) 
      
      #Launch location of drone. 
      ## It is hardcoded here. The puprose of this code is to simulate 
-     lnch_lon= -77.873886
-     lnch_lat=34.241753
+     lnch_lon= -77.874886
+     lnch_lat=34.211753
      
      R = 6378.1 #Radius of the Earth
      brng = Geodesic.WGS84.Inverse(lon, lat, lnch_lon, lnch_lat)['azi1']  # Calculate the bearing of the travel
@@ -110,50 +100,39 @@ def drone_sim(lon,lat): # Lon, Lat>>braodcasted. lonb,latb>> Where the subject i
 
 
 
-
-
-
-
-
-#Current drone location 
 a=-77.870654
 b= 34.221926 
 
-### Lets define list of how x direction speed and y direction speed change through the code
+
 xspeed=[0]
 yspeed=[0]
-
-# Guess ranges.
 maxlon,maxlat,minlon,minlat= guessrange(a,b) ## This function i wrote to calculate the max range for a,b above with range of 1 mile.
-
-
 br= drone_sim(maxlon,b)
-xspeed.append(5 * math.cos(br)) 
+xspeed.append(5 * math.cos(br* math.pi / 180)) 
 
 br= drone_sim(minlon,b)
-xspeed.append(5 * math.cos(br))
-
-
-
+xspeed.append(5 * math.cos(br* math.pi / 180))
 if xspeed[1]> xspeed[2]:
-  xspeedlist, lon_fix=findx(minlon,a,b,0,xspeed[2])
+  f=1
+  guess=maxlon
 else:
-  xspeedlist, lon_fix=findx(maxlon,a,b,1,xspeed[1])
-  
- 
+  f=0
+  guess=minlon
+
+print(findx( guess,a,b,f))
+
 br= drone_sim(a,maxlat)
-yspeed.append(5 * math.sin(br)) 
+yspeed.append(5 * math.sin(br* math.pi / 180)) 
 
 br= drone_sim(a,minlat)
-yspeed.append(5 * math.sin(br))
-
-
-
+yspeed.append(5 * math.sin(br* math.pi / 180))
 if yspeed[1]> yspeed[2]:
-  yspeedlist, lat_fix=findy(minlat,a,b,0)
+  f=1
+  guess=maxlat
 else:
-  yspeedlist, lat_fix=findx(maxlat,a,b,1)
-  
+  f=0
+  guess=minlat
+ 
 
-  
-print(xspeedlist)
+print(findy( guess,a,b,f))
+
